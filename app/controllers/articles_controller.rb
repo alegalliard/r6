ad469e55c9532b445904cc05f3dbeefa64baf588
Alessandra Dias de Souza <alegalliard@MacBook-Pro-de-Alessandra.local>
2019-10-20 22:08:13 -0300

    partials, add comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,18 +4,46 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
 
     def create
         #render plain: params[:article].inspect #renderiza dahoramente o objeto json
         @article = Article.new(article_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+
+#        @article.save
+#        redirect_to @article
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
 
     private
